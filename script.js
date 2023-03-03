5e95592a80de93a07c90c265add88fda9c1fec18
Abhijithu5 <126268217+Abhijithu5@users.noreply.github.com>
2023-03-04 04:38:44 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var stringVariable = 'HEY';
+alert(stringVariable);
+var num1 = 5;
+var num2 = 6;
+var num3 = num1 + num2;
+alert(num3);
+
+
+function add() {
+    var num11 = 10;
+    var num22 = 6;
+    var num33 = num11 + num22;
+    alert(num33);
+    
+}
+add();
